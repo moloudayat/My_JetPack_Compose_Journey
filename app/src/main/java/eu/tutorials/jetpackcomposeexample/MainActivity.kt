8386b24e1cd036e1fd4a76c7d1e7c47ac82c35b3
moloud <moloud.ayat@gmail.com>
2025-11-09 19:18:37 +0330

    [ADD] add onClick and state

--- app/src/main/java/eu/tutorials/jetpackcomposeexample/MainActivity.kt
+++ app/src/main/java/eu/tutorials/jetpackcomposeexample/MainActivity.kt
@@ -1,6 +1,7 @@
 package eu.tutorials.jetpackcomposeexample
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,9 +12,11 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Face
@@ -26,6 +29,10 @@ import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -47,9 +54,20 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp() {
+    var count = remember {
+        mutableStateOf(0)
+    }
+
+    var anotherCount by remember {
+        mutableStateOf(0)
+    }
+
+    val scrollState = rememberScrollState()
+
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .verticalScroll(scrollState) // ✅ Make it scrollable
             .padding(20.dp),
         verticalArrangement = Arrangement.spacedBy(25.dp),
         horizontalAlignment = Alignment.CenterHorizontally
@@ -131,6 +149,25 @@ fun MyApp() {
         ) {
             Text(text = "Simple Button")
         }
+
+        Spacer(modifier = Modifier.size(25.dp))
+        Button(onClick = {
+            count.value++
+            Log.d("Count", "${count.value}")
+        }) {
+            Text(text = "count = ${count.value}")
+        }
+        Text(text = "count = ${count.value}")
+
+        Spacer(modifier = Modifier.size(25.dp))
+        Button(onClick = {
+            anotherCount++
+            Log.d("Another Count", "${anotherCount}")
+        }) {
+            Text(text = "Another Count = ${anotherCount}")
+        }
+        Text(text = "Another Count = ${anotherCount}")
+
 
     }
 }
